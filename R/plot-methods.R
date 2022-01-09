81fcba88799f478f39f225279b7f2013adf657e1
xvtyzn <ide_0109@outlook.jp>
2022-01-09 15:13:36 +0900

    new

---
+++ R/plot-methods.R
@@ -0,0 +1,314 @@
+#############################################
+# orthologの結果を表示させる
+#############################################
+# 系統樹と対応して、orthologの有無の表示
+# いつものやつ。aplotで作成する
+#############################################
+#' Title
+#'
+#' @param polaris polaris object
+#' @param label
+#' @param num_ortho
+#' @param
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' @importFrom aplot
+#'
+plot_ortho <- function(polaris, label, num_ortho){
+
+  host <- label #メタデータから取得するようにしたいよね
+
+  element <- polaris # polarisのorthogorupsからorthogroups genecountを抽出
+  #あるなしに変更
+  # できれば、各Orthologの数の分布をしたにだしたいところ
+  element[element > 1] <- 1
+  cols <- colnames(element)
+
+  element_filtered <- element %>%
+    rownames_to_column("OG") %>%
+    group_by(across(all_of(cols))) %>%
+    summarise(freq = n()) %>%
+    ungroup() %>%
+    rowid_to_column() %>%
+    dplyr::arrange(desc(freq)) %>%
+    dplyr::filter(freq > num_ortho) # ある一定数以上の表示
+
+  gg_element <- element_filtered %>%
+    dplyr::mutate(freq2 = as.factor(freq)) %>%
+    transform(freq3 = freq * -1) %>%
+    tidyr::pivot_longer(c(-rowid, -freq, -freq2, -freq3, -Total), values_to = "presence") %>%
+    dplyr::mutate(host = rep(host, 43)) # ここをどうにかして、自動化しないと
+
+  gg_element %>%
+    ggplot(aes(x = reorder(rowid, freq3), y = name)) +
+    geom_point(aes(size=ifelse(presence==0, NA, presence), fill = host),
+               shape = 21) +
+    labs( x= "Presence of Orthologus", y = "") +
+    theme_minimal() +
+    guides(size=FALSE) +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank()) -> ortholog_presence
+
+  gg_element %>%
+    dplyr::distinct(rowid,freq2, .keep_all = TRUE) %>%
+    ggplot(aes(x = reorder(rowid, freq3), y = freq)) +
+    geom_bar(stat = "identity") +
+    geom_text(aes(label=freq), vjust=0) +
+    theme_minimal() +
+    labs(x= "") +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank()) -> ortholog_sum
+
+
+  ortho_tree <- ortholog_presence %>%
+    insert_top(ortholog_sum, height = .5) %>%
+    insert_left(ortholog_tree)
+}
+
+#############################################
+# 取れたゲノムの系統組成を示す
+#############################################
+# gtdbtkの結果を基にして、系統組成を作成する
+# metadataがある時はmetadataを用いて分ける
+#
+#############################################
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_gtdbtk <- function(gtdbtk, metadata, type = c("single", "ani", "pplacer"),
+                        taxonomy = c("domain", "phylum",
+                                     "class", "order", "family", "genus",
+                                     "species"), ...){
+
+  # gtdbtk <- polaris # polarisからgtdbtkの結果を抽出
+  gtdbtk_df <- gtdbtk
+
+  taxonomy <- paste(taxonomy, "gtdb", sep = "_")
+
+  if(!is_null(metadata)){
+    metadata <- polaris # polarisからmetadataを抜く
+  }
+
+  gtdbtk_meta <- gtdbtk_df %>%
+    left_join(metadata, by = "user_genome") %>% # x軸用のメタデータの取得
+
+  gtdbtk_gg <- gtdbtk_meta %>%
+    group_by(sample, !!!taxonomy) %>%
+    summarise(count_genome = n()) %>%
+    arrange(desc(count_genome))
+
+  color_order <- gtdbtk_meta %>%
+    group_by(taxnomy) %>%
+    summarise(count_genome = n()) %>%
+    arrange(desc(count_genome)) %>%
+    select(genus_gtdb) %>% unlist()
+
+  gtdbtk_gg %>%
+    ggplot(aes(x = sample, y =count_genome, fill = genus_gtdb)) +
+    geom_bar(stat = "identity") + theme_minimal() +
+    scale_fill_manual(values=colors, limits = color_order)
+
+}
+
+
+#############################################
+# Checkmの結果、いつもの図を作る
+#############################################
+# checkmの結果を基にしていつもの図を作る
+# completenessとcontamination
+#
+#############################################
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_checkm <- function(checkm, metadata = NULL, order = NULL, ...){ # いつかinputをpolarisへ直す
+
+  quality_gg <- checkm %>%
+    ggplot() +
+    geom_point(aes(x = reorder(Bin_Id, -Completeness), y = Completeness, colour= "Completeness")) +
+    geom_point(aes(x = Bin_Id, y = Contamination, colour= "Contamination")) +
+    theme_minimal() +
+    scale_colour_manual(values = c("red", "blue")) +
+    scale_y_continuous(sec.axis = sec_axis(~.*5, name = "Contamination [%]")) +
+    labs(y = "Completeness [%]", x = "The number of SAGs", color = "type")
+
+
+  print(quality_gg)
+}
+
+#############################################
+# 系統樹とcheckm, gtdbtkの結果を統合したものを作成
+#############################################
+# ggtreeExtraを使った可視化をする
+#############################################
+
+plot_qcphylo <- function(polaris, layout = "fan",
+                         legend.size = 0.8,
+                         open.angle = 10,
+                         fill.element = NULL,
+                         bar.element = NULL,
+                         phylo_color = NULL,
+                         bar_color = NULL,
+                         metadata_column = NULL){
+
+
+
+}
+
+# 以下のはdraft versionこれを上記の形に変えていく
+plot_overview <- function(phylo, gtdb, quality_df, checkm, metadata, legend.size = 0.8){
+  m_p <- ggtree(phylo, layout="fan", branch.length = "none",
+                size=0.4, open.angle=10, aes(color=group)) +
+    scale_color_manual(values=colors,
+                       guide=guide_legend(keywidth = legend.size,
+                                          keyheight = legend.size, order=4)) +
+    new_scale_color()
+
+  m_p1 <- m_p %<+%
+    gtdb + geom_fruit(data=metadata, geom=geom_tile,
+                      mapping=aes(y=user_genome,
+                                  fill=spcimen_id),
+    ) +
+    scale_fill_manual(values=colors,
+                      guide=guide_legend(keywidth = legend.size,
+                                         keyheight = legend.size, order=4)) +
+    new_scale_fill() +
+    geom_fruit(data=quality_df, geom=geom_tile,
+               mapping=aes(y=Bin_Id, fill=quality),
+    ) +
+    scale_fill_manual(values=colors,
+                      guide=guide_legend(keywidth = legend.size,
+                                         keyheight = legend.size, order=4)) +
+    new_scale_fill()
+
+  m_p2 <- m_p1 + geom_fruit(data=checkm, geom=geom_bar,
+                            mapping=aes(y=Bin_Id, x=Completeness, fill=species_gtdb),
+                            orientation="y",
+                            stat="identity",
+                            axis.params=list(
+                              axis       = "x",
+                              text.size  = 1,
+                              hjust      = -0.5,
+                              text.angle  = 315,
+                            )
+  ) +
+    scale_fill_manual(values=colors,
+                      guide=guide_legend(keywidth = legend.size,
+                                         keyheight = legend.size, order=4))+
+    geom_fruit(data=checkm, geom=geom_bar,
+               mapping=aes(y=Bin_Id, x=Contamination, fill=species_gtdb),
+               orientation="y",
+               stat="identity",
+               axis.params=list(
+                 axis       = "x",
+                 text.size  = 1,
+                 hjust      = -0.5,
+                 text.angle  = 315,
+               )
+    ) +
+    scale_fill_manual(values=colors,
+                      guide=guide_legend(keywidth = 0.8,
+                                         keyheight = 0.8, order=4))+
+    geom_treescale(fontsize=2, linesize=0.3, x=4.9, y=0.1) +
+    theme(legend.position=c(0.95, 0.5),
+          legend.background=element_rect(fill=NA),
+          legend.title=element_text(size=6.5),
+          legend.text=element_text(size=4.5),
+          legend.spacing.y = unit(0.02, "cm"),
+    )
+  return(m_p2)
+}
+
+#############################################
+# ANIの結果を可視化する
+#############################################
+# ゲノム間のANIの結果を可視化する
+# defaultはヒートマップになっている
+#
+# optionによって、サブクラスごとのANIの分布にすることも可能
+#
+#############################################
+#' Title
+#'
+#' @param polaris
+#' @param plot_type
+#' @param subcluster
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_ani <- function(polaris, plot_type = c("heatmap", "distribution"),
+                     subcluster = FALSE, ...){
+
+  ani <- polaris # aniのデータを抽出する
+
+
+
+  print()
+
+}
+
+#############################################
+# AAIの結果を可視化する
+#############################################
+# ゲノム間のAAIの結果を可視化する
+# defaultはヒートマップになっている
+#
+# optionによって、サブクラスごとのAAIの分布にすることも可能
+#
+#############################################
+#' Title
+#'
+#' @param polaris
+#' @param plot_type
+#' @param subcluster
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_aai <- function(polaris, plot_type = c("heatmap", "distribution"),
+                     subcluster = FALSE, ...){
+
+  aai <- polaris # aaiの結果を抽出する
+
+}
+
+#############################################
+# ゲノム間の距離を比較する
+#############################################
+# ゲノム間の距離を比較する
+# 比較できるのは
+# 系統距離: phyloから
+# ANI: aniから
+# AAI: aaiから
+# gene content: Orthogroupsから
+#############################################
+# GGallyみたいな比較の図を書きたいね
+#############################################
+
+plot_distance <- function(polaris){
+
+  aai <- polaris # aaiの結果を抽出する
+
+}
+
+#############################################
+# thresholdを設定した際に、どの様なcluster数になるかplotする
+#############################################
+#
+#############################################
+
---
+++ R/read-methods.R
@@ -0,0 +1,530 @@
+#############################################
+# fastaファイルをまとめて読み込む
+#############################################
+# 関数をまとめて読みこませるgeneral関数を作った方がいい気がする
+#############################################
+#' Read fasta files from specific directory
+#'
+#' fastaファイルをまとめて読み込むための関数
+#'
+#' @param fastas fastaのファイルパスのリストもしくはディレクトリ
+#' @param dir (default: FALSE)
+#' @param type 配列のタイプ, DNAかAAか
+#' @param extension fastaファイルの拡張子。読み込みの際に指定された拡張子はファイル名から削除されて、ゲノム名となる
+#' @param names ゲノム の名前を自分で指定したい場合に指定する
+#'
+#' @importFrom Biostrings readDNAStringSet
+#' @importFrom stringr str_split
+#' @importFrom stringr str_replace_all
+#' @importFrom purrr map
+#'
+#' @return DNAStringSetと名前のリスト
+#' @export
+#'
+#' @examples
+#' #fasta_dir <- system.file("inst/extdata/Rickettsiales/fasta", package = "polaris")
+#' #fasta_list <- read_fastas(fasta_dir, dir = TRUE, type = "DNA")
+#'
+#' # fastaのファイル一覧取得。こちらでも動く
+#' # fasta_filelist <- list.files(path = fastas, pattern = NULL, full.names = T)
+#' # fasta_list <- read_fastas(fasta_filelist)
+read_fastas <- function(fastas, dir = FALSE, type =c("DNA", "AA"),
+                        extension = "fna", names = NULL, ...){
+
+  # ディレクトリか否か
+  if (isTRUE(dir)){
+    # ファイルの個々のリストを取得する
+    fasta_list <- list.files(path = fastas, pattern = NULL, full.names = T)
+  } else {
+    fasta_list <- fastas
+  }
+
+  # ゲノムの名前を自分で指定するかどうか
+  # 要素の数で確認はしていないので、少し挙動が不安
+  if(!is_null(names)){
+    genome_names <- names
+  } else {
+    name_list <- fasta_list %>% str_split("/", simplify = T)
+    genome_names <- name_list[,ncol(name_list)] %>%
+      str_replace_all(pattern=paste0(".",extension), replacement="") %>%
+      str_replace_all(pattern=" ", replacement="_")
+  }
+
+  # BiostringsのreadDNAまたはreadAAを用いてまとめてmapで取得
+  # 中身はBiostringのXstring classのlist
+  genomefastas <- fasta_list %>%
+    map(readDNAStringSet)
+
+  names(genomefastas) <- genome_names
+  genome_list <- list(genome = genomefastas, names = genome_names)
+  # fastaファイル自体とそれぞれのgenomeの名前を定義する
+  # listの要素名に指定する
+  return(genome_list)
+}
+
+#############################################
+# gffの読み込み
+#############################################
+#
+# gffファイルを読み込む
+# 現在は真核生物は非対応。エクソンの処理ができない
+# 将来的に対応してもいいがそれよりやることは多い
+#
+#############################################
+#' Read gff files from specific directory
+#'
+#' gffファイルを一つ読み込むための関数
+#'
+#' @param file gffファイルのパス
+#' @param tibble (default: FALSE) 出力をgffclassではなくtibbleにする
+#'
+#' @importFrom readr read_tsv
+#' @importFrom tidyr separate
+#' @importFrom tidyr unnest
+#' @importFrom tidyr pivot_wider
+#'
+#'
+#' @return gff gffのクラス
+#' @export
+#'
+#' @examples
+#'
+read_gff <- function(file, tibble = FALSE, extension = "gff"){
+
+  colnames <- c("seqid", "source", "type", "start", "end", "score", "strand", "phase","attributes")
+  coltype <- list("c", "c", "c", "d", "d", "c", "c", "c", "c") # 一文字じゃないといけない
+
+  name <- file %>% str_split("/", simplify = T)
+  genome_names <- name[,ncol(name)] %>%
+    str_replace(pattern=paste0(".",extension), replacement="") %>%
+    str_replace(pattern=" ", replacement="_")
+
+  # 行の頭が#の場合は適切な形で処理する
+  # prokkaのgffには末尾にfastaファイルがあるらしい (なんで？？？)
+  # これらの処理をするためにna.omitしている。もっといい方法があるように思うが、、、
+  gff <- read_tsv(pipe(paste("grep -v \\#", file)), col_names = colnames, col_types = coltype) %>% # 行の頭が#の場合は適切な形で処理する
+    na.omit() %>% #warningの原因、fasta部分を削除
+    mutate(attributes_list = strsplit(attributes, ";")) %>% # tag情報などを分割して
+    unnest(cols = attributes_list) %>% #一行づつの対応にして、
+    separate(attributes_list, into = c("attributes_id", "elements"), sep = "=") %>% # 列名となるものと要素を分解し
+    pivot_wider(names_from = attributes_id, values_from = elements, values_fill = NA) # 列名と要素をいれる
+
+  gff_name <- gff %>%
+    mutate(genome = genome_names)
+
+  # 返り値はgffのデータ
+  return(gff_name)
+}
+
+#############################################
+# gffの読み込み
+#############################################
+#
+# まとめて、gffを読み込む
+#
+#############################################
+#' 複数のgffファイルの読み込み
+#'
+#' 複数のgffファイルを一つ一つ読み込んで統合することが少々煩雑なので、
+#' 最初から複数ファイルを読み込むための関数を提供する
+#'
+#' @importFrom
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_gffs <- function(files, dir = FALSE, extension = "gff", names = NULL){
+
+  # ディレクトリか否か
+  if (isTRUE(dir)){
+    # ファイルの個々のリストを取得する
+    file_list <- list.files(path = files, pattern = NULL, full.names = T)
+  } else {
+    file_list <- files
+  }
+
+  # gffの名前を自分で指定するかどうか
+  # 要素の数で確認はしていないので、少し挙動が不安
+  if(!is_null(names)){
+    gff_names <- names
+  } else {
+    name_list <- file_list %>% str_split("/", simplify = T)
+    gff_names <- name_list[,ncol(name_list)] %>%
+      str_replace_all(pattern=paste0(".",extension), replacement="") %>%
+      str_replace_all(pattern=" ", replacement="_")
+  }
+
+  # BiostringsのreadDNAまたはreadAAを用いてまとめてmapで取得
+  # 中身はBiostringのXstring classのlist
+  gff_output <- file_list %>%
+    map(read_gff) %>%
+    bind_rows()
+
+  # # gffファイル自体とそれぞれのgenomeの名前を定義する
+  # names(gff_list) <- gff_names
+  #   # listをgenomefastaと定義し直して、return
+  # gff_output <- list(gff = gff_list, names = gff_names)
+
+  return(gff_output)
+}
+
+#############################################
+# Orthofinderの結果を読み込む
+#############################################
+# Orthogroups
+#############################################
+#' Orthofinderの結果を読み込む
+#'
+#' Orthofinderの結果を読み込む。出力としては3つある。
+#' 1つ目はどの遺伝子とどのOrthogroupが対応しているか
+#' 2つ目はどのOrthogroupがどのゲノムにどれくらいあるか
+#' 3つ目はシングルコピーオルソログはどれか
+#'
+#' @param ortho_file orthofinderの結果、orthofroups.tsv
+#'
+#' @return ortho_class ortho
+#' @export
+#'
+#' @examples
+read_ortho <- function(ortho_file){
+  ortho_tb <- read_tsv(ortho_file)
+
+  # カンマを含んでいる全ての要素はリストに分解したい
+  ortho_list <- ortho_tb %>%
+    mutate_all(~str_replace_all(.," ", "")) %>% #Use `mutate_at(df, vars(-group_cols()), myoperation)` to silence the message.
+    mutate_all(~str_split_list(.,pattern = ",")) %>% #Use `mutate_at(df, vars(-group_cols()), myoperation)` to silence the message.
+    unchop(Orthogroup) %>%
+    pivot_longer(-Orthogroup) %>%
+    unnest(-Orthogroup) %>%
+    na.omit()
+
+  ortho_count <- ortho_list %>%
+    group_by(Orthogroup, name) %>%
+    mutate(count = n()) %>%
+    select(-value) %>%
+    distinct() %>% #重複を削除
+    pivot_wider(id_cols = Orthogroup, names_from = name, values_from = count) %>% #行をOG, 列をgenomeに
+    mutate_all(~replace(., is.na(.), 0)) #NAを置換
+
+  singlecopy <- ortho_count %>% #single copy marker geneの抽出が必要
+    column_to_rownames("Orthogroup") %>%
+    filter_all(all_vars(. == 1)) %>% #全ての要素が1であれば
+    rownames()
+
+  ortho <- list(ortho_genes = ortho_list, ortho_count = ortho_count, singlecopy = singlecopy)
+  return(ortho)
+}
+
+#############################################
+# Checkmの結果を読み込む
+#############################################
+# checkmの結果を読み込む
+# tsvがinputであれば、TRUEとしてそうでなければ、
+# tsvじゃないものをinput (ちょっと面倒くさい)
+#############################################
+#############################################
+
+read_checkm <- function(file){
+
+  # gtdbtkのNAをRのNAへ変換する
+  checkm_data <- read_tsv(file) %>%
+    rename_all(~str_replace_all(.,"#","")) %>%
+    rename_all(~str_replace_all(.,"^ ","_")) %>%
+    rename_all(~str_replace_all(.," ","_")) # closest_placement
+
+  return(checkm_data)
+
+}
+
+#############################################
+# GTDBtkの結果を読み込む
+#############################################
+# gtdbtk.bac120.summary.tsvをinputにする
+# 基本的にはtaxonomyは分解される
+#
+#############################################
+#' GTDBtkのtsvを読み込む
+#'
+#' @param file gtdbtk.bac120.summary.tsv
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' #file <- system.file("inst/extdata/Rickettsiales/gtdbtk/gtdbtk.bac120.summary.tsv", package = "polaris")
+#' #gtdb <- read_gtdbtk(file)
+#'
+#' # polaris %<>%
+#' #  add_polaris(gtdb)
+#' #
+read_gtdbtk <- function(file){
+
+  # gtdbtkのNAをRのNAへ変換する
+  gtdb_data <- read_tsv(file) %>%
+    na_if("N/A")
+
+  taxonomy_names <- c("domain", "phylum",
+                      "class", "order",
+                      "family", "genus",
+                      "species")
+
+  refined_gtdb <- gtdb_data %>%
+    separate(classification, paste(taxonomy_names, "gtdb", sep = "_"), sep = ";") %>% # classification
+    separate(fastani_taxonomy, paste(taxonomy_names, "ani", sep = "_"), sep = ";") %>% # fastani_taxonomy
+    separate(closest_placement_taxonomy, paste(taxonomy_names, "place", sep = "_"), sep = ";")  %>% #pplacer
+    mutate_all(~str_replace_all(.,"^.__","")) # closest_placement
+
+  return(refined_gtdb)
+}
+
+#############################################
+# GTDBtkの結果を読み込む
+#############################################
+# gtdbtk.bac120.summary.tsvをinputにする
+# 基本的にはtaxonomyは分解される
+#
+#############################################
+#' Eggnong annotationの読み込み
+#'
+#' Eggnogデータを操作しやすい形で読み込む
+#' おそらく、懸念点としては、各cell内でカンマで
+#' 分けられてるデータをリスト化すること
+#'
+#' @param file eggnongの結果、eggnog annotation
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_eggnog <- function(file, name = NULL, extension = ".annotations"){
+  eggnog <- read_tsv(file, skip = 3) %>%
+    rename_all(~str_replace_all(.," ","_"))   # スペースを_へ
+
+  if(is_null(name)){
+    name <- file %>% str_split("/", simplify = T)
+    genome_names <- name[,ncol(name)] %>%
+      str_replace(pattern=paste0(".",extension), replacement="") %>%
+      str_replace(pattern=" ", replacement="_")
+  } else {
+    genome_names <- name
+  }
+
+  # 各列において、カンマを含む列を文字列からリストへ
+  refined_eggnong <- eggnog %>%
+    mutate(GOs = str_split_list(GOs, pattern = ","),
+           EC = str_split_list(EC, pattern = ","),
+           KEGG_ko = str_split_list(KEGG_ko, pattern = ","),
+           KEGG_Pathway = str_split_list(KEGG_Pathway, pattern = ","),
+           KEGG_Module = str_split_list(KEGG_Module, pattern = ","),
+           KEGG_Reaction = str_split_list(KEGG_Reaction, pattern = ","),
+           KEGG_rclass = str_split_list(KEGG_rclass, pattern = ","),
+           BRITE = str_split_list(BRITE, pattern = ","),
+           KEGG_TC = str_split_list(KEGG_TC, pattern = ","),
+           CAZy = str_split_list(CAZy, pattern = ","),
+           BiGG_Reaction = str_split_list(BiGG_Reaction, pattern = ","),
+           eggNOG_OGs = str_split_list(eggNOG_OGs, pattern = ","),
+           COG_Functional_cat. = str_split_list(COG_Functional_cat., pattern = "")
+    ) %>%
+    mutate(genome = genome_names)
+  #
+  return(refined_eggnong)
+}
+
+# カンマを含む要素をリストへ分解する
+str_split_list <- function(cols, pattern = ","){
+  cols %>%
+    str_split(pattern = pattern) %>%
+    as.list()
+}
+
+#############################################
+# GTDBtkの結果を読み込む
+#############################################
+# gtdbtk.bac120.summary.tsvをinputにする
+# 基本的にはtaxonomyは分解される
+#
+#############################################
+#' Eggnogデータのまとめて読み込み
+#'
+#' Eggnong-mapperの結果をまとめて読み込む
+#'
+#' @param eggnog
+#' @param dir
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+read_eggnogs <- function(eggnog, dir = FALSE, extension = "annotations", names = NULL){
+
+  # ディレクトリか否か
+  if (isTRUE(dir)){
+    # ファイルの個々のリストを取得する
+    eggnog_files <- list.files(path = eggnog, pattern = NULL, full.names = T)
+  } else {
+    eggnog_files <- eggnog
+  }
+
+  eggnog_list <- eggnog_files %>%
+    map(read_eggnog, name = names, extension = extension) %>%
+    bind_rows()
+
+  return(eggnog_list)
+}
+
+#############################################
+# Interproscanの結果を読み込む
+#############################################
+# Inteproscan
+# Protein accession (e.g. P51587)
+# Sequence MD5 digest (e.g. 14086411a2cdf1c4cba63020e1622579)
+# Sequence length (e.g. 3418)
+# Analysis (e.g. Pfam / PRINTS / Gene3D)
+# Signature accession (e.g. PF09103 / G3DSA:2.40.50.140)
+# Signature description (e.g. BRCA2 repeat profile)
+# Start location
+# Stop location
+# Score - is the e-value (or score) of the match reported by member database method (e.g. 3.1E-52)
+# Status - is the status of the match (T: true)
+# Date - is the date of the run
+# InterPro annotations - accession (e.g. IPR002093)
+# InterPro annotations - description (e.g. BRCA2 repeat)
+# (GO annotations (e.g. GO:0005515) - optional column; only displayed if –goterms option is switched on)
+# (Pathways annotations (e.g. REACT_71) - optional column; only displayed if –pathways option is switched on)
+#############################################
+#' interproscanの読み込み
+#'
+#' 基本的にはtsvを読み込むことにする
+#' 複数のゲノムがある場合には、
+#'
+#' @param file interproscanファイル
+#' @param type ファイルのタイプ3パターン
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_interpro <- function(file, type = c("tsv", "json", "xml")){
+
+  if(type == "tsv") {
+    colnames <- c("protein_accession", "md5", "length",
+                  "database", "db_accession", "db_annotation",
+                  "start", "end", "e_values", "status",
+                  "date", "interpro_accession", "interpro_desc",
+                  "GO", "Pathway")
+
+    interpro <- read_tsv(file, col_names = colnames) %>% #1行1タンパク質にしたい
+      group_nest(protein_accession, md5, length) #何でnestするのかは要検討
+
+  } else if (type == "json") {
+
+  } else if (type == "xml") {
+
+  } else {
+    stop("読み込みの形式が不明です。指定してください")
+  }
+
+  return(interpro)
+}
+
+#############################################
+# interproscanのファイルをまとめて読み込み
+#############################################
+# interproscanの結果をまとめて読み込むための
+# ツール。
+#############################################
+#' Title
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+read_interpros <- function(files, dir = FALSE, type = c("tsv", "json", "xml"),
+                           names = NULL){
+
+  # ディレクトリか否か
+  if (isTRUE(dir)){
+    # ファイルの個々のリストを取得する
+    file_list <- list.files(path = files, pattern = ".tsv", full.names = T)
+  } else {
+    file_list <- files
+  }
+
+  interpro_data <- file_list %>%
+    map(read_interpro, type = type) %>%
+    bind_rows()
+
+  return(interpro_data)
+}
+
+read_ani <- function(file, extension = "fna"){
+
+  ani <- file %>%
+    read_tsv() %>%
+    mutate(seq1 = str_replace(seq1, pattern=paste0(".",extension), replacement=""),
+           seq2 = str_replace(seq2, pattern=paste0(".",extension), replacement="")) %>%
+    pivot_wider(names_from = seq1, values_from = ANI) %>%
+    column_to_rownames("seq2")
+
+  return(ani)
+}
+
+read_aai <- function(file, extension = "faa"){
+
+  aai <- file %>%
+    read_tsv() %>%
+    mutate(seq1 = str_replace(seq1, pattern=paste0(".",extension), replacement=""),
+           seq2 = str_replace(seq2, pattern=paste0(".",extension), replacement="")) %>%
+    pivot_wider(names_from = seq1, values_from = AAI) %>%
+    column_to_rownames("seq2")
+
+  return(aai)
+}
+
+read_tree <- function(file){
+
+  phylo <- file %>%
+    read_tree()
+
+  return(phylo)
+
+}
+
+#############################################
+# サンプルデータの読み込み
+#############################################
+# サンプルメタデータを読み込ませる
+# colnamesは
+# 1. genome
+# 2. 以下任意 (想定としてはsubclassなどが入る)
+#
+#############################################
+#' metaデータの読み込み
+#'
+#' ゲノムのメタデータの読み込みを行う
+#'
+#' @param file tsvまたはcsvファイル
+#'
+#' @return sample_data
+#' @export
+#'
+#' @examples
+#'
+#'
+read_metadata <- function(file){
+
+  sample_data <- read_tsv(file)
+
+  if (TRUE) { #genome列が必要。列名に含んでいるかどうかを確かめる
+    stop("genome列がありません。メタデータにはgenome列が必須です")
+  }
+
+  return(sample_data)
+
+}
